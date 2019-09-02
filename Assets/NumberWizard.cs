7a9bdcbb8e7136c4e732e4c25d617abdaf4180a9
chellapandim <chellapandi.murugan@imaginea.com>
2019-09-02 14:04:58 +0530

    Learnt basics of Unity

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Test");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Enter hit");
+        }
+    }
+}
